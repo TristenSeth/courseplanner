ae80e7929344b77d249966e56c902e1a2592e369
Tristen Nollman <tristen.nollman@gmail.com>
2021-09-06 18:57:05 -0700

    initial commit

--- ec.py
+++ ec.py
@@ -1,10 +1,6 @@
 from explorecourses import *
 from explorecourses import filters
 
-
-
-
-
 def getCourseInfo(yearOffered, courseQuery, quarter):
     connect = CourseConnection()
     courses = connect.get_courses_by_query(courseQuery, quarter.toUpper(), year=yearOffered)
